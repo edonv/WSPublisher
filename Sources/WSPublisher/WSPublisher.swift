f08af2df6baed9d8258081ff93f0f856e607b120
Edon <edon@valdman.works>
2023-09-04 10:51:54 -0500

    Moved `URLSessionWebSocketDelegate` code
    Moved `WebSocketPublisher`'s `URLSessionWebSocketDelegate` extension code to separate file.

--- Sources/WSPublisher/WSPublisher.swift
+++ Sources/WSPublisher/WSPublisher.swift
@@ -166,28 +166,6 @@ public class WebSocketPublisher: NSObject {
     }
 }
 
-// MARK: - WebSocketPublisher: URLSessionWebSocketDelegate
-
-extension WebSocketPublisher: URLSessionWebSocketDelegate {
-    /// This function is called automatically by the delegate system when the WebSocket connection
-    /// opens successfully.
-    public func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didOpenWithProtocol protocol: String?) {
-        let event = WSEvent.connected(`protocol`)
-        _subject.send(event)
-        startListening()
-    }
-    
-    /// This function is called automatically by the delegate system when the WebSocket connection
-    /// is closed.
-    public func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
-        clearTaskData()
-        
-        let reasonStr = reason != nil ? String(data: reason!, encoding: .utf8) : nil
-        let event = WSEvent.disconnected(closeCode, reasonStr)
-        _subject.send(event)
-    }
-}
-
 // MARK: - Companion Types
 
 extension WebSocketPublisher {
---
+++ Sources/WSPublisher/WebSocketPublisher+URLSessionDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  WebSocketPublisher+URLSessionDelegate.swift
+//  
+//
+//  Created by Edon Valdman on 9/4/23.
+//
+
+import Foundation
+import Combine
+
+extension WebSocketPublisher: URLSessionWebSocketDelegate {
+    /// This function is called automatically by the delegate system when the WebSocket connection
+    /// opens successfully.
+    public func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didOpenWithProtocol protocol: String?) {
+        let event = WSEvent.connected(`protocol`)
+        _subject.send(event)
+        startListening()
+    }
+    
+    /// This function is called automatically by the delegate system when the WebSocket connection
+    /// is closed.
+    public func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
+        clearTaskData()
+        
+        let reasonStr = reason != nil ? String(data: reason!, encoding: .utf8) : nil
+        let event = WSEvent.disconnected(closeCode, reasonStr)
+        _subject.send(event)
+    }
+}
