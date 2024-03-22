ad9340461c4b4849adcd55c75c6fb6b9dc5ef54e
Edon <edon@valdman.works>
2024-03-22 13:20:40 -0500

    Added empty privacy manifest

--- Package.swift
+++ Package.swift
@@ -27,7 +27,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "WSPublisher",
-            dependencies: []),
+            dependencies: [],
+            resources: [.copy("../PrivacyInfo.xcprivacy")]
+        ),
         .testTarget(
             name: "WSPublisherTests",
             dependencies: ["WSPublisher"]),
