16be7b4f9d78750f0f3cb811471e1cce19899a7f
Edon <edon@valdman.works>
2024-06-08 14:55:43 -0500

    Update WebSocketPublisher+Errors.swift
    Added a shorthand `typealias` for `WebSocketPublisher.Errors`, as well as a local `typealias` to mark the deprecation.

--- Sources/WSPublisher/Types/WebSocketPublisher+Errors.swift
+++ Sources/WSPublisher/Types/WebSocketPublisher+Errors.swift
@@ -7,7 +7,13 @@
 
 import Foundation
 
+/// A non-namespaced shorthand for ``WebSocketPublisher/Errors``.
+public typealias WSErrors = WebSocketPublisher.Errors
+
 extension WebSocketPublisher {
+    @available(*, deprecated, renamed: "Errors", message: "WSErrors has been renamed to `Errors` for the sake of brevity.")
+    public typealias WSErrors = Errors
+    
     /// Errors pertaining to ``WebSocketPublisher``.
     public enum Errors: Error {
         /// Thrown when there is no active connection.
