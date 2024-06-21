2a9992407706560856dc231a85bf7ef44640188b
Edon <edon@valdman.works>
2024-06-21 11:35:12 -0500

    Update WebSocketPublisher+URLSessionDelegate.swift
    Moved `clearTaskData()` into a `defer` block.

--- Sources/WSPublisher/Extensions/WebSocketPublisher+URLSessionDelegate.swift
+++ Sources/WSPublisher/Extensions/WebSocketPublisher+URLSessionDelegate.swift
@@ -20,7 +20,7 @@ extension WebSocketPublisher: URLSessionWebSocketDelegate {
     /// This function is called automatically by the delegate system when the WebSocket connection
     /// is closed.
     public func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
-        clearTaskData()
+        defer { clearTaskData() }
         
         let reasonStr = reason != nil ? String(data: reason!, encoding: .utf8) : nil
         let event = Event.disconnected(closeCode, reasonStr)
