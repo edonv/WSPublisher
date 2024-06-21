0c3701eaa35cf7d44afd5f407a0ab564b617ff1f
Edon <edon@valdman.works>
2024-06-21 13:26:40 -0500

    Update WebSocketPublisher+Event.swift
    Refactored `.disconnected` case to include more data. For convenience and a smooth transition to updated case associated values, additional static functions have been added.

--- Sources/WSPublisher/Types/WebSocketPublisher+Event.swift
+++ Sources/WSPublisher/Types/WebSocketPublisher+Event.swift
@@ -25,7 +25,7 @@ extension WebSocketPublisher {
         case connected(_ protocol: String?, upgradeHeaders: HTTPFields)
         
         /// Occurs when the connection is closed.
-        case disconnected(_ closeCode: URLSessionWebSocketTask.CloseCode, _ reason: String?)
+        case disconnected(Disconnect)
         
         /// Occurs when ``WebSocketPublisher/webSocketTask`` receives a `Data` message.
         case data(Data)
@@ -37,5 +37,47 @@ extension WebSocketPublisher {
         /// [`URLSessionWebSocketTask.Message`](https://developer.apple.com/documentation/foundation/urlsessionwebsockettask/message)
         /// being made with the possibility of new cases.
         case generic(URLSessionWebSocketTask.Message)
+        
+        /// Occurs when the connection is closed.
+        public static func disconnected(_ closeCode: URLSessionWebSocketTask.CloseCode, _ reason: String?) -> Event {
+            .disconnected(.closeCode(closeCode, reason))
+        }
+        
+        /// The connection closes with a [`URLError`](https://developer.apple.com/documentation/foundation/urlerror), instead of with a close code.
+        ///
+        /// # Examples
+        /// Common scenarios include:
+        /// - Attemping to establish a connection with a URL that doesn't support a WebSocket connection.
+        /// - Attemping to establish a connection that fails the WebSocket handshake.
+        /// - ~~A connected WebSocket server closes without sending a close code.~~
+        ///     - This scenario is being internally mapped to a ``disconnected(_:_:)`` type.
+        public static func disconnected(_ urlError: URLError) -> Event {
+            .disconnected(.urlError(urlError))
+        }
+        
+        /// Occurs when the connection is closed.
+        public static func disconnected(_ error: Error) -> Event {
+            .disconnected(.unknownError(error))
+        }
+    }
+}
+
+extension WebSocketPublisher.Event {
+    public enum Disconnect: Sendable {
+        /// The connection closes safely with a close code, and optionally with a descriptive reason.
+        case closeCode(_ closeCode: URLSessionWebSocketTask.CloseCode, _ reason: String?)
+        
+        /// The connection closes with an error, instead of with a close code.
+        ///
+        /// # Examples
+        /// Common scenarios include:
+        /// - Attemping to establish a connection with a URL that doesn't support a WebSocket connection.
+        /// - Attemping to establish a connection that fails the WebSocket handshake.
+        /// - ~~A connected WebSocket server closes without sending a close code.~~
+        ///     - This scenario is being internally mapped to a ``disconnected(_:_:)`` type.
+        case urlError(URLError)
+        
+        /// Some other type of `Error` occurs that isn't a `URLError`.
+        case unknownError(Error)
     }
 }
